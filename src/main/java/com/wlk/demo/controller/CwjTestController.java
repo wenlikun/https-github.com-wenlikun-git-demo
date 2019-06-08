cd181fbef5f0356c73770ee50b55623c1cc6c43a
wenlikun <1370605410@qq.com>
2019-06-08 10:43:23 +0800

    Test

---
+++ src/main/java/com/wlk/demo/controller/CwjTestController.java
@@ -0,0 +1,15 @@
+package com.wlk.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class CwjTestController {
+
+    @RequestMapping(value = "test" , name = "测试")
+    public String demo(){
+        return "test";
+    }
+
+
+}
