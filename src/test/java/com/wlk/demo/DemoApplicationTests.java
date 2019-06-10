cce326f2e634923f377eb5c945531a15dafc7755
wenlikun <gg454595@>
2019-06-10 22:33:09 +0800

    aa

--- src/test/java/com/wlk/demo/DemoApplicationTests.java
+++ src/test/java/com/wlk/demo/DemoApplicationTests.java
@@ -2,15 +2,33 @@ package com.wlk.demo;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class DemoApplicationTests {
 
+    @Autowired
+    DataSource dataSource;
+
+
     @Test
-    public void contextLoads() {
+    public void test() throws Exception{
+        System.out.println(dataSource.getClass());
+        Connection connection = dataSource.getConnection();
+        System.out.println(connection);
+        connection.close();
+    }
+
+
+    @Test
+    public void test2(){
+        System.out.println(1);
     }
 
 }
